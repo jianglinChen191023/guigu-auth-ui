89033ffb645be3a8d54d3ce1ab4e1971e5d68b2a
jianglinChen191023 <1912623646@qq.com>
2022-11-30 20:04:54 +0800

    首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <div v-for="project in projects">
+      <el-link type="primary" :href="project.path">{{ project.name }}</el-link>
+      <br/>
+    </div>
   </div>
 </template>
 
@@ -13,6 +17,34 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  created() {
+    this.projects = [
+      {
+        name: '尚筹网会员系统',
+        path: this.basePath
+      },
+      {
+        name: '尚筹网后台管理系统',
+        path: `${this.basePath}:8080/atcrowdfunding02-admin-webui-1.0-SNAPSHOT/admin/to/login/page.html`
+      },
+      {
+        name: '电商后台管理系统',
+        path: `${this.basePath}:81/#/login`
+      },
+      {
+        name: '通用权限系统',
+        path: `${this.basePath}:1127/#/login`
+      }
+    ]
+  },
+  data() {
+    return {
+      // 服务器地址
+      basePath: 'http://175.178.174.83',
+      // 项目
+      projects: []
+    }
   }
 }
 </script>
